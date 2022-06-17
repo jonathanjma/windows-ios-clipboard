d1d49ce63e3b984db172a4efcacdb90cc384aac3
Jonathan Ma <jonathanjma03@gmail.com>
2022-06-17 16:17:46 -0700

    multiple user support almots done

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,47 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const {log, error} = require('firebase-functions/lib/logger');
 
-admin.initializeApp()
+const express = require('express');
+const authMiddleware = require('./authMiddleware');
 
-exports.get_latest = functions.https.onRequest((req, res) => {
-    admin.database().ref('/').get().then((data) => {
-        res.status(200).send({'latest_value': data.val()['latest_value']})
+/*
+https://stackoverflow.com/questions/66688858/use-headers-and-basic-authentication-in-python-requests
+https://imgur.com/a/msGAhWW (shortcuts)
+*/
+
+admin.initializeApp();
+
+const app = express();
+app.use(authMiddleware);
+
+app.post('/api', async (req, res) => {
+    res.status(200).send(
+        {'name': 'Windows-iOS Clipboard', 'version': '2.0', 'author': 'Jonathan Ma'}
+    );
+})
+
+app.post('/get', async (req, res) => {
+    let username = req.user['email'].split('@')[0]
+    log(username)
+
+    admin.database().ref('/' + username).get().then(data => {
+        res.status(200).send(
+            {'latest_value': data.exists() ? data.val()['latest_value'] : ''}
+        )
     });
 });
 
-exports.push_data = functions.https.onRequest((req, res) => {
-    let value_in = req.query.value
-    admin.database().ref('/').set({
+app.post('/push', async (req, res) => {
+    let username = req.user['email'].split('@')[0]
+    let value_in = req.body.value
+    log(username, value_in)
+
+    admin.database().ref('/' + username).set({
         latest_value: value_in
     }).then(() => {
         res.status(200).send({'latest_value': value_in});
     });
 });
+
+exports.api = functions.https.onRequest(app);
--- python/windows_script.py
+++ python/windows_script.py
@@ -3,26 +3,50 @@ import requests
 import pystray
 import PIL.Image
 import time
+from datetime import datetime
+
+username = 'sus_elephant@gmail.com'
+password = 'sussyamogus'
 
 def get_clipboard():
-    global icon
-    clip_response = requests.get('https://win-ios-clipboard.web.app/get').json()['latest_value']
-    icon.notify('Received Item: ' + clip_response, title='Windows-iOS Clipboard')
-    print(clip_response)
-    win32clipboard.OpenClipboard()
-    win32clipboard.EmptyClipboard()
-    win32clipboard.SetClipboardText(clip_response)
-    win32clipboard.CloseClipboard()
+    global icon, username, password
+    start = datetime.now()
+    clip_response = requests.post('https://win-ios-clipboard.web.app/get', auth=(username, password))
+    if clip_response.status_code == 200:
+        clip_response = clip_response.json()['latest_value']
+        print(f'Clipboard Get took {(datetime.now()-start).microseconds/1000} ms')
+
+        print(clip_response)
+        icon.notify('Received Item: ' + clip_response[:200], title='Windows-iOS Clipboard')
+
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardText(clip_response)
+        win32clipboard.CloseClipboard()
+    else:
+        print(f'Error {clip_response.status_code}')
+        icon.notify('**An error has occurred** \nMake sure your email and password have been entered correctly',
+                    title='Windows-iOS Clipboard')
 
 def push_clipboard():
-    global icon
+    global icon, username, password
     win32clipboard.OpenClipboard()
     clip_in = win32clipboard.GetClipboardData()
-    clip_in = clip_in.replace(' ', '%20')
     win32clipboard.CloseClipboard()
-    clip_response = requests.get('https://win-ios-clipboard.web.app/push?value=' + clip_in).json()['latest_value']
-    icon.notify('Copied Item: ' + clip_response, title='Windows-iOS Clipboard')
-    print(clip_response)
+
+    start = datetime.now()
+    clip_response = requests.post('https://win-ios-clipboard.web.app/push', auth=(username, password),
+                                  data={'value': clip_in})
+    if clip_response.status_code == 200:
+        clip_response = clip_response.json()['latest_value']
+        print(f'Clipboard Push took {(datetime.now()-start).microseconds/1000} ms')
+
+        print(clip_response)
+        icon.notify('Copied Item: ' + clip_response[:200], title='Windows-iOS Clipboard')
+    else:
+        print(f'Error {clip_response.status_code}')
+        icon.notify('**An error has occurred** \nMake sure your email and password have been entered correctly',
+                    title='Windows-iOS Clipboard')
 
 def quit():
     global icon

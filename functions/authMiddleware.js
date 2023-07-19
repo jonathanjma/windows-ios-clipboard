1b704ef5f0da3226c601cba54f78dd1e20393e2c
Jonathan Ma <jonathanjma03@gmail.com>
2023-07-18 21:47:55 -0400

    Upgrade functions to v2, email validation, code formatting

--- functions/authMiddleware.js
+++ functions/authMiddleware.js
@@ -1,48 +1,53 @@
-const bcrypt = require("bcrypt")
-const getUuid = require('uuid-by-string');
-const {getFirestore} = require("firebase-admin/firestore");
+const bcrypt = require("bcrypt");
+const getUuid = require("uuid-by-string");
+const { getFirestore } = require("firebase-admin/firestore");
 
 module.exports = async (req, res, next) => {
-
-    if (!req.headers.authorization || !req.headers.authorization.startsWith('Basic ')) {
-        console.log("No Authorization header")
-        return res.status(403).send('Unauthorized')
+    if (!req.headers.authorization || !req.headers.authorization.startsWith("Basic ")) {
+        return res.status(403).send("Unauthorized");
     }
 
-    const decoded_auth = atob(req.headers.authorization.split('Basic ')[1]).split(':')
-    const email = decoded_auth[0].toLowerCase()
-    const password = decoded_auth[1]
+    const decoded_auth = atob(req.headers.authorization.split("Basic ")[1])
+        .split(":");
+    const email = decoded_auth[0].toLowerCase();
+    const password = decoded_auth[1];
 
     // retrieve user with matching email
-    const query =
-        await getFirestore().collection('users').where('email', '==', email).get();
+    const query = await getFirestore()
+        .collection("users")
+        .where("email", "==", email)
+        .get();
 
-    const signIn = !query.empty
-    console.log((signIn ? 'signing in ' : 'registering ') + email)
+    const signIn = !query.empty;
+    console.log((signIn ? "signing in " : "registering ") + email);
 
     // sign-in if matching user found, otherwise register user
     if (signIn) {
         // save data and ref to prevent duplicate queries in API endpoint
-        req.data = query.docs[0].data()
-        req.ref = query.docs[0].ref
+        req.data = query.docs[0].data();
+        req.ref = query.docs[0].ref;
         // compare password hashes
-        const success = await bcrypt.compare(password, req.data['password_hash'])
+        const success = await bcrypt.compare(password, req.data["password_hash"]);
         if (!success) {
-            console.error('Incorrect email or password')
-            return res.status(403).send('Unauthorized')
+            return res.status(403).send("Unauthorized");
         }
-    } else { // register user
+    } else {
+        // validate email
+        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if (!emailRegex.test(email.toLowerCase())) {
+            return res.status(400).send("Invalid email");
+        }
         // create password hash
-        const hash = await bcrypt.hash(password, 10)
+        const hash = await bcrypt.hash(password, 10);
         // validate email?
         req.data = {
             email: email,
             password_hash: hash,
             file: false,
-            value: ''
-        }
-        req.ref = await getFirestore().collection('users').add(req.data);
+            value: "",
+        };
+        req.ref = await getFirestore().collection("users").add(req.data);
     }
-    req.data['uid'] = getUuid(email) // store uuid (used in filenames for file copying)
-    next()
-}
+    req.data["uid"] = getUuid(email); // store uuid (used in filenames for file copying)
+    next();
+};
--- functions/index.js
+++ functions/index.js
@@ -1,89 +1,88 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-const {Storage} = require('@google-cloud/storage');
+const { onRequest } = require("firebase-functions/v2/https");
+const admin = require("firebase-admin");
+const { Storage } = require("@google-cloud/storage");
+const FileType = require("file-type");
+const express = require("express");
 
-const express = require('express')
-const authMiddleware = require('./authMiddleware')
+const authMiddleware = require("./authMiddleware");
 const formMiddleware = require("./formMiddleware");
-const FileType = require('file-type');
 
-// https://cloud.google.com/nodejs/docs/reference/storage/latest
-// https://firebase.google.com/docs/functions/2nd-gen-upgrade
-
-admin.initializeApp()
+admin.initializeApp();
 const storage = new Storage();
-const bucketName = 'win-ios-clipboard.appspot.com'
+const bucketName = "win-ios-clipboard.appspot.com";
 
-const app = express()
+const app = express();
 
 // Get API info endpoint
-app.get('/api', async (req, res) => {
-    res.status(200).send(
-        {'name': 'Windows-iOS Clipboard', 'version': '3.0', 'author': 'Jonathan Ma'}
-    )
-})
+app.get("/api", async (req, res) => {
+    res.status(200)
+        .send({
+            name: "Windows-iOS Clipboard",
+            version: "3.0",
+            author: "Jonathan Ma",
+        });
+});
 
 // Paste clipboard content endpoint
-app.get('/paste', authMiddleware, async (req, res) => {
-    console.log('pasting clipboard')
+app.get("/paste", authMiddleware, async (req, res) => {
 
     // get user data from database (set by authMiddleware)
-    const data = req.data
+    const data = req.data;
 
     // determine whether to return text value or file
-    if (!data['file']) {
-        return res.status(200).send({'value': data['value']})
+    if (!data["file"]) {
+        return res.status(200).send({ value: data["value"] });
     } else {
         // download file from Storage
-        const contents = await storage.bucket(bucketName).file(data['uid']).download();
-        const fileBuf = contents[0]
-        const mimeType = await FileType.fromBuffer(fileBuf)
-        console.log(mimeType)
+        const contents = await storage.bucket(bucketName).file(data["uid"]).download();
+        const fileBuf = contents[0];
+        const mimeType = await FileType.fromBuffer(fileBuf);
+        console.log(mimeType);
         // set MIME type and file extension
-        res.set('Content-Type', mimeType['mime'])
-        res.set('File-Extension', mimeType['ext'])
-        return res.status(200).send(fileBuf)
+        res.set("Content-Type", mimeType["mime"]);
+        res.set("File-Extension", mimeType["ext"]);
+        return res.status(200).send(fileBuf);
     }
-})
+});
 
 // Copy clipboard content endpoint
-app.post('/copy', authMiddleware, formMiddleware, async (req, res) => {
-   console.log('copying clipboard')
+app.post("/copy", authMiddleware, formMiddleware, async (req, res) => {
 
-    const data = req.data // set by authMiddleware
+    const data = req.data; // set by authMiddleware
 
     // helper function for updating stored value in database
     const updateDb = async (value, file) => {
-        await req.ref.update({
-            file: file,
-            value: value
-        }).catch(error => {
-            return res.status(500).send('Data could not be saved.' + error)
-        })
-        return res.status(200).send({'value': value});
-    }
+        await req.ref
+            .update({
+                file: file,
+                value: value,
+            }).catch((error) => {
+                return res.status(500).send("Data could not be saved." + error);
+            });
+        return res.status(200).send({ value: value });
+    };
 
     // prioritize text value over file if both are sent
-    const textValue = req.body['value']
+    const textValue = req.body["value"];
     if (textValue !== undefined) {
-        await updateDb(textValue, false)
+        await updateDb(textValue, false);
     } else if (req.files.length !== 0) {
-        let file = req.files[0]
+        let file = req.files[0];
         // console.log(file)
 
         // upload file to Storage using the user's ID as the filename
-        await storage.bucket(bucketName).file(data['uid']).save(file['buffer'])
+        await storage.bucket(bucketName).file(data["uid"]).save(file["buffer"])
             .catch((e) => {
                 console.log(e);
-                return res.status(500).send('File upload failed.');
+                return res.status(500).send("File upload failed.");
             });
 
         // update db with name of stored file
-        await updateDb(file['fileName'], true)
+        await updateDb(file["fileName"], true);
     } else {
         // if no text value or file sent
-        return res.status(400).send('Value is required.')
+        return res.status(400).send("Value is required.");
     }
-})
+});
 
-exports.api = functions.https.onRequest(app)
+exports.api = onRequest(app);
